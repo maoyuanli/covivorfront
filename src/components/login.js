62c8df56a05dabcf236e71990f076d39ecabf6aa
mao <maoyuan@gmail.com>
2020-07-20 13:59:20 -0400

    login and register works with backend

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
+import axios from 'axios';
+import {Link} from "react-router-dom";
 
 const Login = () => {
+
+    const [formData, setFormData] = useState({
+        email: '',
+        pass: '',
+    });
+
+    const {email, pass} = formData;
+
+    const handleOnChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value})
+    };
+
+    const handleOnSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+            const body = JSON.stringify({username: email, password: pass});
+            const res = await axios.post('http://localhost:3000/api/user/login', body, config)
+            console.log(res.data)
+        } catch (e) {
+            console.log(e)
+        }
+
+    };
+
     return (
-        <div>
-            Login
-        </div>
+        <Fragment>
+            <h1 className="large text-primary">Sign In</h1>
+            <p className="lead"><i className="fas fa-user"></i> Log into your account</p>
+            <form className="form" onSubmit={handleOnSubmit}>
+                <div className="form-group">
+                    <input type="email" placeholder="Email Address" name="email"
+                           value={email} onChange={e => handleOnChange(e)}
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="pass"
+                        minLength="6"
+                        required
+                        value={pass} onChange={e => handleOnChange(e)}
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Login"/>
+            </form>
+            <p className="my-1">
+                Don't have an account? <Link to='/register'>Register</Link>
+            </p>
+        </Fragment>
     );
 };
 
--- src/components/register.js
+++ src/components/register.js
@@ -1,10 +1,88 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
+import axios from 'axios';
+import {Link} from "react-router-dom";
 
 const Register = () => {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        pass1: '',
+        pass2: ''
+    });
+
+    const {name, email, pass1, pass2} = formData;
+
+    const handleOnChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value})
+    };
+
+    const handleOnSubmit = async (e) => {
+        e.preventDefault();
+        if (pass1 !== pass2) {
+            alert('passwords not match')
+        } else {
+            const newUser = {
+                username: email,
+                fullname: name,
+                password: pass1
+            }
+            try {
+                const config = {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+                const body = JSON.stringify(newUser);
+                const res = await axios.post('http://localhost:3000/api/user/register', body, config)
+                console.log(res.data)
+            } catch (e) {
+                console.log(e)
+            }
+        }
+    };
+
     return (
-        <div>
-            Register
-        </div>
+        <Fragment>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+            <form className="form" onSubmit={handleOnSubmit}>
+                <div className="form-group">
+                    <input type="text" placeholder="Name" name="name" required
+                           value={name} onChange={e => handleOnChange(e)}
+                    />
+                </div>
+                <div className="form-group">
+                    <input type="email" placeholder="Email Address" name="email"
+                           value={email} onChange={e => handleOnChange(e)}
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="pass1"
+                        minLength="6"
+                        required
+                        value={pass1} onChange={e => handleOnChange(e)}
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="pass2"
+                        minLength="6"
+                        required
+                        value={pass2} onChange={e => handleOnChange(e)}
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Register"/>
+            </form>
+            <p className="my-1">
+                Already have an account? <Link to='login'>Sign In</Link>
+            </p>
+        </Fragment>
     );
 };
 
