e5ffbce7f917ba11bc7a6dabbddddd91f5649aa4
mao <maoyuan@gmail.com>
2020-07-20 15:37:21 -0400

    alerts redux

--- src/components/alert.js
+++ src/components/alert.js
@@ -2,12 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux';
 
-const Alert = ({alerts}) =>
-    alerts && alerts.length > 0 && alerts.map(alert => (
-        <div key={alert.id} className={`alert alert-${alert.alertType}`}>
-            {alert.msg}
-        </div>
-    ));
+const Alert = ({alerts}) => {
+    if (alerts && alerts.length > 0)
+        return alerts.map(alert =>
+            (
+                <div key={alert.id} className={`alert alert-${alert.alertType}`}>
+                    {alert.msg}
+                </div>
+            )
+        );
+    return null;
+}
 
 const mapStateToProps = state => ({
     alerts: state.alertReducer
