8aa08efdd704c1e8a63c4b686625de2390345318
mao <maoyuan@gmail.com>
2020-07-23 14:45:17 -0400

    profiles bare bone finished

--- src/components/profile-item.js
+++ src/components/profile-item.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Button, Icon, Item, Message} from "semantic-ui-react";
+import {Icon, Item, Message} from "semantic-ui-react";
 import {Link} from "react-router-dom";
 
 const ProfileItem = ({
@@ -16,30 +16,14 @@ const ProfileItem = ({
             <Item.Image size='tiny' src={hostPhotoURL}/>
 
             <Item.Content>
-                <Item.Header as={Link} to={{pathname: '/profile', profileProps: {id: _id}}}>{fullname}</Item.Header>
+                <Item.Header as={Link} to={{
+                    pathname: '/profile',
+                    profileProps: {id: _id, photoUrl: hostPhotoURL}
+                }}>{fullname}</Item.Header>
                 <Message
                     info
                     header={bio}
                 />
-                <Item.Meta>
-                    <div>
-                        {facebook && (<Button color='facebook'>
-                            <Icon name='facebook'/> {facebook}
-                        </Button>)}
-                        {twitter && (<Button color='twitter'>
-                            <Icon name='twitter'/> {twitter}
-                        </Button>)}
-                        {instagram && (<Button color='instagram'>
-                            <Icon name='instagram'/> {instagram}
-                        </Button>)}
-                        {linkedin && (<Button color='linkedin'>
-                            <Icon name='linkedin'/> {linkedin}
-                        </Button>)}
-                        {youtube && (<Button color='youtube'>
-                            <Icon name='youtube'/> {youtube}
-                        </Button>)}
-                    </div>
-                </Item.Meta>
                 <Item.Extra><Icon className='map marker'/>{location}</Item.Extra>
             </Item.Content>
         </Item>
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,28 +1,76 @@
-import React, {useEffect} from 'react';
+import React, {Fragment, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from "react-redux";
 import {getProfileAction} from "../redux/action/profile-action";
-import {withRouter} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
+import {Button, Grid, Header, Icon, Image, Message} from "semantic-ui-react";
 
 const Profile = props => {
     useEffect(() => {
         getProfileAction();
     }, [getProfileAction])
 
-    const profilePassed = props.location.profileProps;
-    if (!profilePassed) props.history.push('/allprofiles')
-    else {
-        const profileParsed = props.profile.profiles.filter(p => p._id === props.location.profileProps.id)
-        console.log(profileParsed[0])
-        console.log(props.auth.user._id)
-        console.log(profileParsed[0].user._id === props.auth.user._id)
+    let curProfile = null;
+    let profileBelongsCurUser = false
+
+    const propsPassed = props.location.profileProps;
+    if (!propsPassed) {
+        props.history.push('/allprofiles')
+    } else {
+        const profilePassed = props.profile.profiles.filter(p => p._id === props.location.profileProps.id)[0];
+        curProfile = profilePassed;
+        if (props.auth.user) {
+            profileBelongsCurUser = profilePassed.user._id === props.auth.user._id
+        }
+
+        return (
+            <Fragment>
+                {profileBelongsCurUser && (
+                    <div className="dash-buttons">
+                        <Link to="/upsert-profile" className="ui primary button"
+                        ><Icon className="edit"/> Edit Profile</Link
+                        >
+                    </div>
+                )}
+
+                <Grid>
+                    <Grid.Column width={5}>
+                        <Image src={propsPassed.photoUrl}/>
+                    </Grid.Column>
+                    <Grid.Column width={10}>
+                        <Header as='h1'>{curProfile.user.fullname}</Header>
+                        <Message
+                            info
+                            header={curProfile.bio}
+                            content={curProfile.location}
+                        />
+
+                        <div>
+                            {curProfile.facebook && (<Button color='facebook'>
+                                <Icon name='facebook'/> {curProfile.facebook}
+                            </Button>)}
+                            {curProfile.twitter && (<Button color='twitter'>
+                                <Icon name='twitter'/> {curProfile.twitter}
+                            </Button>)}
+                            {curProfile.instagram && (<Button color='instagram'>
+                                <Icon name='instagram'/> {curProfile.instagram}
+                            </Button>)}
+                            {curProfile.linkedin && (<Button color='linkedin'>
+                                <Icon name='linkedin'/> {curProfile.linkedin}
+                            </Button>)}
+                            {curProfile.youtube && (<Button color='youtube'>
+                                <Icon name='youtube'/> {curProfile.youtube}
+                            </Button>)}
+                        </div>
+                    </Grid.Column>
+                </Grid>
+
+
+            </Fragment>
+        );
     }
 
-    return (
-        <div>
-            profile
-        </div>
-    );
+    return null
 };
 
 Profile.propTypes = {
