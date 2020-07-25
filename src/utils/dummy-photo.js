9f864f5d6754becafe345a040c623e2f619c142e
mao <maoyuan@gmail.com>
2020-07-24 23:24:40 -0400

    add props to dummy-photo.js

--- src/utils/dummy-photo.js
+++ src/utils/dummy-photo.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {Image} from "semantic-ui-react";
 
 const DummyPhoto = React.memo(
-    function generatePhoto() {
+    function generatePhoto({size}) {
         const randInt = Math.floor(Math.random() * 100);
         const randGender = randInt > 50 ? 'women' : 'men'
         const photoUrl = `https://randomuser.me/api/portraits/${randGender}/${randInt}.jpg`;
         return (
-            <Image src={photoUrl} size='small' circular/>
+            <Image src={photoUrl} size={size} circular/>
         )
     }
 );
