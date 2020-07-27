dd90f06956fdc7e2eb9831b1ea111c998605d1f5
mao <maoyuan@gmail.com>
2020-07-27 16:25:20 -0400

    scroll to top

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import AllProfiles from "./components/profile/all-profiles";
 import Profile from "./components/profile/profile";
 import AllPosts from "./components/post/all-posts";
 import Post from "./components/post/post";
+import ScrollToTop from "./utils/scroll-to-top";
 
 if (localStorage.token) {
     setAuthToken(localStorage.token)
@@ -29,23 +30,25 @@ export const App = () => {
     return (
         <Provider store={store}>
             <BrowserRouter>
-                <Fragment>
-                    <Navbar/>
-                    <Route exact path='/' component={Landing}/>
-                    <section className='container'>
-                        <Alert/>
-                        <Switch>
-                            <Route exact path='/register' component={Register}/>
-                            <Route exact path='/login' component={Login}/>
-                            <Route exact path='/allprofiles' component={AllProfiles}/>
-                            <Route exact path='/profile' component={Profile}/>
-                            <Route exact path='/allposts' component={AllPosts}/>
-                            <Route exact path='/post' component={Post}/>
-                            <PrivateRoute exact path='/dashboard' component={Dashboard}/>
-                            <PrivateRoute exact path='/upsert-profile' component={UpsertProfile}/>
-                        </Switch>
-                    </section>
-                </Fragment>
+                <ScrollToTop>
+                    <Fragment>
+                        <Navbar/>
+                        <Route exact path='/' component={Landing}/>
+                        <section className='container'>
+                            <Alert/>
+                            <Switch>
+                                <Route exact path='/register' component={Register}/>
+                                <Route exact path='/login' component={Login}/>
+                                <Route exact path='/allprofiles' component={AllProfiles}/>
+                                <Route exact path='/profile' component={Profile}/>
+                                <Route exact path='/allposts' component={AllPosts}/>
+                                <Route exact path='/post' component={Post}/>
+                                <PrivateRoute exact path='/dashboard' component={Dashboard}/>
+                                <PrivateRoute exact path='/upsert-profile' component={UpsertProfile}/>
+                            </Switch>
+                        </section>
+                    </Fragment>
+                </ScrollToTop>
             </BrowserRouter>
         </Provider>
     );
